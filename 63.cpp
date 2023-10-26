8c5284642340c6edbd9b580b32a013daed411088
ThisIsGagan19 <148439123+ThisIsGagan19@users.noreply.github.com>
2023-10-26 14:33:35 +0530

    Update 63.cpp

--- 63.cpp
+++ 63.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-// Define a class to represent a graph
+
 class Graph {
 public:
     int numVertices;
@@ -13,13 +13,13 @@ public:
         adjList.resize(vertices);
     }
 
-    // Function to add an undirected edge to the graph
+
     void addEdge(int src, int dest) {
         adjList[src].push_back(dest);
         adjList[dest].push_back(src);
     }
 
-    // Recursive function for DFS traversal
+
     void DFSUtil(int vertex, vector<bool>& visited) {
         visited[vertex] = true;
         cout << vertex << " ";
@@ -31,7 +31,7 @@ public:
         }
     }
 
-    // Function to perform DFS traversal
+
     void DFS(int startVertex) {
         vector<bool> visited(numVertices, false);
         DFSUtil(startVertex, visited);
@@ -42,7 +42,7 @@ int main() {
     int numVertices = 7;
     Graph graph(numVertices);
 
-    // Add edges to the graph
+
     graph.addEdge(0, 1);
     graph.addEdge(0, 2);
     graph.addEdge(1, 3);
@@ -50,7 +50,7 @@ int main() {
     graph.addEdge(2, 5);
     graph.addEdge(2, 6);
 
-    // Perform DFS traversal starting from vertex 0
+
     cout << "Depth-First Search (DFS) starting from vertex 0: ";
     graph.DFS(0);
     cout << endl;
