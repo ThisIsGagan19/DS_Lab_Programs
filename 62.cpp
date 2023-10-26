e2e2f00d1f67a274b987674cc1d1de9da5e72418
ThisIsGagan19 <148439123+ThisIsGagan19@users.noreply.github.com>
2023-10-26 14:34:39 +0530

    Update 62.cpp

--- 62.cpp
+++ 62.cpp
@@ -3,7 +3,7 @@
 #include <queue>
 using namespace std;
 
-// Define a class to represent a graph
+
 class Graph {
 public:
     int numVertices;
@@ -14,13 +14,13 @@ public:
         adjList.resize(vertices);
     }
 
-    // Function to add an undirected edge to the graph
+
     void addEdge(int src, int dest) {
         adjList[src].push_back(dest);
         adjList[dest].push_back(src);
     }
 
-    // Function to perform BFS traversal
+
     void BFS(int startVertex) {
         vector<bool> visited(numVertices, false);
         queue<int> q;
@@ -47,7 +47,7 @@ int main() {
     int numVertices = 7;
     Graph graph(numVertices);
 
-    // Add edges to the graph
+
     graph.addEdge(0, 1);
     graph.addEdge(0, 2);
     graph.addEdge(1, 3);
@@ -55,7 +55,7 @@ int main() {
     graph.addEdge(2, 5);
     graph.addEdge(2, 6);
 
-    // Perform BFS traversal starting from vertex 0
+
     cout << "Breadth-First Search (BFS) starting from vertex 0: ";
     graph.BFS(0);
     cout << endl;
